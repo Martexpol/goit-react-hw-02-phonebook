ad4f9d8cc5ef8be580c558f4630990e47149dc2f
Martexpol <150294627+Martexpol@users.noreply.github.com>
2024-06-07 18:55:54 +0200

    .

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import styles from "./App.module.scss";
+
+function App() {
+  return (
+    <>
+      <div className={styles.main}>
+        {" "}
+        <h1 className={styles.mainHeader}>React Homework Template 1 (Vite)</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
